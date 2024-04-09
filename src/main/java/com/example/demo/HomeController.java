2e35a62e7c4ba8d9d948413a863c7ed7b7664ccb
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-04-09 16:51:20 -0500

    Usando thymeleft

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HomeController {
     
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
         return "home";
     }
